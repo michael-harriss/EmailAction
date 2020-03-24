5fada61d13e53f845ffcf9d9d3dab392371a3ab8
Michael Harris <59890915+michael-harriss@users.noreply.github.com>
2020-03-24 16:05:30 -0500

    Update entrypoint.sh

--- ActionDir/entrypoint.sh
+++ ActionDir/entrypoint.sh
@@ -1,4 +1,8 @@
 #!/bin/sh -l
 
+apt install ssmtp
 apt  install mutt
+sudo apt install ssmtp
+sudo apt install mutt
 echo “testing message” | mutt -s "Testing Email" michael.harris@abbvie.com
+echo "Subject: hello" | sendmail test@example.com
