bed7ffa28f386d0286458cb68b1ec44f3b0fae2e
Michael Harris <59890915+michael-harriss@users.noreply.github.com>
2020-04-14 14:37:22 -0500

    Update entrypoint.sh

--- ActionDir/entrypoint.sh
+++ ActionDir/entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/sh -l
 
-echo "The Email Action directory has had a push" | mail -s "subject" $INPUT_EMAIL
+sh -c "echo "The Email Action directory has had a push" | mail -s "subject" $INPUT_EMAIL"
